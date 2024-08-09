88d499d5a336372015332314f77138019b5bc801
Tongei <tann.tongei@gmail.com>
2024-08-09 10:23:41 +0700

    New push

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 app.use(express.json());
+
+app.use(cors({ origin: 'http://127.0.0.1:5500' }));
+
 const employee = require('./route/employee.route');
 const customer = require('./route/customer.route');
 
